8f233b5b75645eb4f39a2f479b6566f2f353ff4d
bruce <zhenyue.li@baidao.com>
2013-04-30 17:58:47 +0800

    fix backbone products collection and view

--- public/js/collections/products.js
+++ public/js/collections/products.js
@@ -6,7 +6,7 @@ define([
 ], function($, _, Backbone, dictModel){
   var ProductCollection = Backbone.Collection.extend({
     model: dictModel,
-    url: "products",
+    url: "products.json",
     initialize: function(){
 
     },
